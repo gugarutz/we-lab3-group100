36f1f978a5dcf4ceacb79efd91aca5b4501ce2d1
Marc Kaiblinger <e1325247@student.tuwien.ac.at>
2015-05-10 15:56:54 +0200

    language v1.1

---
+++ app/models/Language.java
@@ -0,0 +1,17 @@
+package models;
+
+/**
+ * Created by Marc on 10.05.15.
+ */
+public class Language {
+    public String language;
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+}
